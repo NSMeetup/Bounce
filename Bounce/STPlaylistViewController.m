a5c4ee38cd3b07ebafe5edd9dbf105524774246f
Leah <leah.culver@gmail.com>
2013-03-30 15:48:42 -0700

    styling again

--- Bounce/STPlaylistViewController.m
+++ Bounce/STPlaylistViewController.m
@@ -123,4 +123,79 @@
     [self.navigationController popViewControllerAnimated:YES];
 }
 
+#pragma mark -
+#pragma mark Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    //return [self.friends count];
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *identifier = @"FriendsCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }
+    
+    NSMutableDictionary *track = nil;
+    
+    UILabel *name = [[UILabel alloc] initWithFrame:CGRectMake(80.0, 14.0, self.view.frame.size.width - 100.0, 20.0)];
+    name.font = [UIFont openSansSemiboldWithSize:16.0];
+    name.textColor = [UIColor grayColor];
+    //name.text = [NSString stringWithFormat:@"%@ %@", [friend objectForKey:@"firstName"], [friend objectForKey:@"lastName"]];
+    [cell.contentView addSubview:name];
+    
+    UILabel *description = [[UILabel alloc] initWithFrame:CGRectMake(80.0, 34.0, self.view.frame.size.width - 100.0, 20.0)];
+    description.font = [UIFont openSansLightWithSize:13.0];
+    description.textColor = [UIColor grayColor];
+    //description.text = [NSString stringWithFormat:@"Bounce with %@...", [friend objectForKey:@"firstName"]];
+    [cell.contentView addSubview:description];
+    
+    /*if ([friend objectForKey:@"downloadedIconImage"]) {
+        UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(20.0, 13.0, 40.0, 40.0)];
+        imageView.image = [friend objectForKey:@"downloadedIconImage"];
+        imageView.contentMode = UIViewContentModeScaleAspectFill;
+        imageView.clipsToBounds = YES;
+        imageView.layer.cornerRadius = imageView.frame.size.width / 2;
+        imageView.layer.masksToBounds = YES;
+        [cell.contentView addSubview:imageView];
+    } else {
+        NSURL *imageURL = [NSURL URLWithString:[friend objectForKey:@"icon250"]];
+        NSLog(@"%@", imageURL);
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+            NSData *imageData = [NSData dataWithContentsOfURL:imageURL];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [friend setObject:[UIImage imageWithData:imageData] forKey:@"downloadedIconImage"];
+                [tableView reloadRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationNone];
+            });
+        });
+    }*/
+    
+    return cell;
+}
+
+#pragma mark -
+#pragma mark Table view delegate
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 65.0;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
 @end
